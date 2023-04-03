5035f9089ae74188cc6bc910802ba9daef5a462d
RachelfTech <129456439+RachelfTech@users.noreply.github.com>
2023-04-03 12:18:20 -0400

    Code cleanup

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 
 const audioInputSelector = document.getElementById('audio-input-select');
-const irButton = document.querySelector("#ir-button");
-const currentEffectText = document.querySelector("#current-effect-text");
+const irButton = document.getElementById('ir-button');
+const currentEffectText = document.getElementById('current-effect-text');
 
-const volumeCheckbox = document.querySelector('#volume');
-const wahCheckbox = document.querySelector('#wah');
-const pitchCheckbox = document.querySelector('#pitch');
+const volumeCheckbox = document.getElementById('volume');
+const wahCheckbox = document.getElementById('wah');
+const pitchCheckbox = document.getElementById('pitch');
 
 const loadingSpinner = document.getElementById('loading-spinner');
-console.log(loadingSpinner);
 
 const audioContext = new AudioContext({ latencyHint: 0 });
 const gainNode = audioContext.createGain();
@@ -25,6 +24,7 @@ let irEnabled = true;
 let phaseVocoderNode;
 let lastPitchShiftFactor;
 
+// Whether each effect is active or not.
 const effectStatus = {
   'volume': true,
   'wah': false,
@@ -37,7 +37,7 @@ irButton.addEventListener('click', () => {
   const buttonText = irEnabled ? 'Enable IR' : 'Disable IR';
   irEnabled = !irEnabled;
   irButton.innerHTML = buttonText;
-  setupContext();
+  setupAudioContext();
 });
 
 volumeCheckbox.addEventListener('change', effectChanged);
@@ -55,7 +55,7 @@ function effectChanged(event) {
     }
   }
 
-  setupContext();
+  setupAudioContext();
   let effectText;
   switch (checkbox.id) {
     case 'volume':
@@ -75,44 +75,27 @@ function effectChanged(event) {
 
 
 audioInputSelector.addEventListener('change', audioInputChanged);
-document.body.addEventListener('click', startAudioContext);
+document.body.addEventListener('click', resumeAudioContext);
 
 let guitarAudio;
 
 async function startup() {
-  // Get input devices before setting up the audio context
-  // so proper sound/echo cancelling can be applied depending
+  // Set the input device before setting up the audio context
+  // so that proper sound/echo cancelling can be applied depending
   // on the device type.
   await setInputDevices();
-  setupContext();
+  setupAudioContext();
 }
 
 startup();
 
-async function startFaceTracking() {
-  // get the video
-  video = await getVideo();
-
-  canvas = createCanvas(width, height);
-  ctx = canvas.getContext("2d");
-
-  faceapi = ml5.faceApi(video, detectionOptions, modelReady);
-}
-
-// call app.map.init() once the DOM is loaded
-window.addEventListener("DOMContentLoaded", function () {
-  startFaceTracking();
-});
-
-function modelReady() {
-  console.log("ready!");
-  loadingSpinner.classList.add("hidden");
-  faceapi.detectSingle(gotResults);
-}
-
-function startAudioContext() {
+/** 
+ * The audio context can only be started if the user interacts with the page.
+ * Sometimes it can get into a suspended state and need to be resumed on 
+ * user input.
+ */
+function resumeAudioContext() {
   if (audioContext.state === 'suspended') {
-    console.log('restart');
     audioContext.resume();
   }
 }
@@ -151,8 +134,11 @@ function disconnectEffects(guitarAudio) {
   effects = [];
 }
 
-/** Set up the audio context by getting the input device and connecting it to the output. */
-async function setupContext() {
+/** 
+ * Set up the audio context by connecting all active effects and connecting the 
+ * mic input to the output.
+ */
+async function setupAudioContext() {
   if (guitarAudio) {
     disconnectEffects(guitarAudio);
   }
@@ -177,7 +163,8 @@ async function setupContext() {
   const overdrive = createOverdrive();
 
   await audioContext.audioWorklet.addModule('scripts/phase-vocoder.min.js');
-  phaseVocoderNode = new AudioWorkletNode(audioContext, 'phase-vocoder-processor');
+  phaseVocoderNode =
+    new AudioWorkletNode(audioContext, 'phase-vocoder-processor');
 
   const pitchFactorParam = phaseVocoderNode.parameters.get('pitchFactor');
   pitchFactorParam.value = 1;
@@ -206,7 +193,6 @@ async function setInputDevices() {
   const devices = await navigator.mediaDevices.enumerateDevices();
   devices.forEach((device) => {
     if (device.kind === 'audioinput') {
-      console.log(device);
       const option = document.createElement('option');
       option.text = device.label;
       option.value = device.deviceId;
@@ -218,8 +204,8 @@ async function setInputDevices() {
 async function getMic() {
   // Apply noise cancelling if the current device appears to be a mic instead
   // of an audio interface. Otherwise there is annoying feedback.
-  const isMicrophone =
-    audioInputSelector.options[audioInputSelector.selectedIndex]?.text.toLowerCase().includes('mic');
+  const isMicrophone = audioInputSelector.options[
+    audioInputSelector.selectedIndex]?.text.toLowerCase().includes('mic');
   return navigator.mediaDevices.getUserMedia({
     audio: {
       echoCancellation: isMicrophone,
@@ -232,8 +218,7 @@ async function getMic() {
 }
 
 function audioInputChanged() {
-  console.log(audioInputSelector.value);
-  setupContext();
+  setupAudioContext();
 }
 
 function createOverdrive() {
@@ -245,7 +230,7 @@ function createOverdrive() {
 
 // Copied from: https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/createWaveShaper#examples.
 function makeDistortionCurve(amount) {
-  const k = typeof amount === "number" ? amount : 50;
+  const k = typeof amount === 'number' ? amount : 50;
   const n_samples = 44100;
   const curve = new Float32Array(n_samples);
   const deg = Math.PI / 180;
@@ -260,110 +245,125 @@ function makeDistortionCurve(amount) {
 function createWahWah() {
   const filter = audioContext.createBiquadFilter();
   filter.type = 'bandpass';
-  filter.Q.value = 1; // quality factor of the filter
-  filter.frequency.value = 1000; // center frequency of the filter
+  filter.Q.value = 1;
+  filter.frequency.value = 1000;
 
   return filter;
 }
 
+/** 
+ * The drawing code below is based on the ml5.js FaceApi Video Landmarks example:
+ * https://github.com/ml5js/ml5-library/tree/main/examples/javascript/FaceApi/FaceApi_Video_Landmarks/
+ */
 let faceapi;
 let video;
-let detections;
+let detection;
 const width = 720;
 const height = 540;
 let canvas, ctx;
 
-// by default all options are set to true
 const detectionOptions = {
   withLandmarks: true,
   withDescriptors: false,
 };
 
+async function startFaceTracking() {
+  video = await getVideo();
+
+  canvas = createCanvas(width, height);
+  ctx = canvas.getContext('2d');
+
+  faceapi = ml5.faceApi(video, detectionOptions, modelReady);
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  startFaceTracking();
+});
+
+function modelReady() {
+  loadingSpinner.classList.add('hidden');
+  faceapi.detectSingle(gotResults);
+}
+
+async function getVideo() {
+  // Grab elements, create settings, etc.
+  const videoElement = document.createElement('video');
+  videoElement.setAttribute('style', 'display: none;');
+  videoElement.width = width;
+  videoElement.height = height;
+  document.body.appendChild(videoElement);
+
+  // Create a webcam capture.
+  const capture =
+    await navigator.mediaDevices.getUserMedia({ video: { width, height } });
+  videoElement.srcObject = capture;
+  videoElement.play();
+
+  return videoElement;
+}
+
+function createCanvas(w, h) {
+  const canvas = document.getElementById('main-canvas');
+  canvas.width = w;
+  canvas.height = h;
+  return canvas;
+}
+
 function gotResults(err, result) {
   if (err) {
-    console.log(err);
+    console.error(err);
     return;
   }
 
-  detections = result;
+  // We only care about the first face detected.
+  detection = result[0];
 
-  // Clear part of the canvas
-  ctx.fillStyle = "#000000";
+  ctx.fillStyle = '#000000';
   ctx.fillRect(0, 0, width, height);
 
   ctx.drawImage(video, 0, 0, width, height);
 
-  if (detections) {
-    if (detections.length > 0) {
-      drawBox(detections);
-      drawLandmarks(detections);
-    }
+  if (detection) {
+    drawBox(detection);
+    drawLandmarks(detection);
+    modifyActiveEffects(detection);
   }
   faceapi.detect(gotResults);
 }
 
-function drawBox(detections) {
-  for (let i = 0; i < detections.length; i += 1) {
-    const alignedRect = detections[i].alignedRect;
-    const x = alignedRect._box._x;
-    const y = alignedRect._box._y;
-    const boxWidth = alignedRect._box._width;
-    const boxHeight = alignedRect._box._height;
+/** Draws a box around the detected face. */
+function drawBox(detection) {
+  // Only draw a box around the 
+  const alignedRect = detection.alignedRect;
+  const x = alignedRect._box._x;
+  const y = alignedRect._box._y;
+  const boxWidth = alignedRect._box._width;
+  const boxHeight = alignedRect._box._height;
 
-    ctx.beginPath();
-    ctx.rect(x, y, boxWidth, boxHeight);
-    ctx.strokeStyle = "#13fe32";
-    ctx.lineWidth = 1.5;
-    ctx.stroke();
-    ctx.closePath();
-  }
+  ctx.beginPath();
+  ctx.rect(x, y, boxWidth, boxHeight);
+  ctx.strokeStyle = '#13fe32';
+  ctx.lineWidth = 1.5;
+  ctx.stroke();
+  ctx.closePath();
 }
 
-function drawLandmarks(detections) {
-  for (let i = 0; i < detections.length; i += 1) {
-    const mouth = detections[i].parts.mouth;
-    const nose = detections[i].parts.nose;
-    const leftEye = detections[i].parts.leftEye;
-    const rightEye = detections[i].parts.rightEye;
-    const rightEyeBrow = detections[i].parts.rightEyeBrow;
-    const leftEyeBrow = detections[i].parts.leftEyeBrow;
 
-    drawPart(mouth, true);
-    drawPart(nose, false);
-    drawPart(leftEye, true);
-    drawPart(leftEyeBrow, false);
-    drawPart(rightEye, true);
-    drawPart(rightEyeBrow, false);
+/** Draws facial features on the detected face. */
+function drawLandmarks(detection) {
+  const mouth = detection.parts.mouth;
+  const nose = detection.parts.nose;
+  const leftEye = detection.parts.leftEye;
+  const rightEye = detection.parts.rightEye;
+  const rightEyeBrow = detection.parts.rightEyeBrow;
+  const leftEyeBrow = detection.parts.leftEyeBrow;
 
-
-    // My height range is 15 to 65
-    const mouthHeight = findHeight(mouth);
-
-    maxMouthHeight = Math.max(maxMouthHeight, mouthHeight);
-    minMouthHeight = Math.min(minMouthHeight, mouthHeight);
-
-    // Normalize between 0 to 1
-    const scale = (mouthHeight - minMouthHeight) / (maxMouthHeight - minMouthHeight);
-    // Change either wah frequency or gain based on mouth height.
-    if (effectStatus.wah) {
-      setWahWahFreq(scale);
-    }
-    // Change volume based on mouth height.
-    if (effectStatus.volume) {
-      gainNode.gain.setValueAtTime(scale * 1.5, audioContext.currentTime);
-    }
-    // Change pitch based on mouth height.
-    if (effectStatus.pitch) {
-      const pitchFactorParam = phaseVocoderNode.parameters.get('pitchFactor');
-      const pitchFactor = Math.max(.5, Math.round((scale * 3) * 10) / 10);
-
-      // Only shift the pitch if it has changed a decent amount to avoid constantly changing.
-      if (!lastPitchShiftFactor || Math.abs(lastPitchShiftFactor - pitchFactor) >= .15) {
-        pitchFactorParam.setValueAtTime(pitchFactor, audioContext.currentTime);
-      }
-      lastPitchShiftFactor = pitchFactor;
-    }
-  }
+  drawPart(mouth, true);
+  drawPart(nose, false);
+  drawPart(leftEye, true);
+  drawPart(leftEyeBrow, false);
+  drawPart(rightEye, true);
+  drawPart(rightEyeBrow, false);
 }
 
 function drawPart(feature, closed) {
@@ -385,34 +385,45 @@ function drawPart(feature, closed) {
   ctx.stroke();
 }
 
-// Helper Functions
-async function getVideo() {
-  // Grab elements, create settings, etc.
-  const videoElement = document.createElement("video");
-  videoElement.setAttribute("style", "display: none;");
-  videoElement.width = width;
-  videoElement.height = height;
-  document.body.appendChild(videoElement);
+/** Scales active effects based on the detected mouth height. */
+function modifyActiveEffects(detection) {
+  const mouth = detection.parts.mouth;
 
-  // Create a webcam capture
-  const capture = await navigator.mediaDevices.getUserMedia({ video: { width, height } });
-  videoElement.srcObject = capture;
-  videoElement.play();
+  if (!mouth) {
+    return;
+  }
 
-  return videoElement;
-}
+  // My mouth height range is approximately 15 to 65.
+  const mouthHeight = findHeight(mouth);
 
-function createCanvas(w, h) {
-  const canvas = document.getElementById("main-canvas");
-  canvas.width = w;
-  canvas.height = h;
-  return canvas;
-}
+  maxMouthHeight = Math.max(maxMouthHeight, mouthHeight);
+  minMouthHeight = Math.min(minMouthHeight, mouthHeight);
 
-function setWahWahFreq(value) {
-  const maxFreq = 2200;
-  const newFreq = maxFreq * value;
-  wahWahNode.frequency.setValueAtTime(newFreq, audioContext.currentTime);
+  // Normalize the scale to be between 0 and 1.
+  const scale =
+    (mouthHeight - minMouthHeight) / (maxMouthHeight - minMouthHeight);
+  // Change wah frequency based on mouth height.
+  if (effectStatus.wah) {
+    setWahWahFreq(scale);
+  }
+  // Change volume based on mouth height.
+  if (effectStatus.volume) {
+    gainNode.gain.setValueAtTime(scale * 1.5, audioContext.currentTime);
+  }
+  // Change pitch based on mouth height.
+  if (effectStatus.pitch) {
+    const pitchFactorParam = phaseVocoderNode.parameters.get('pitchFactor');
+    // Set the min pitch factor to .5 because it gets pretty distorted lower.
+    const pitchFactor = Math.max(.5, Math.round((scale * 3) * 10) / 10);
+
+    // Only shift the pitch if it has changed a decent amount since the last
+    // update to avoid constantly changing.
+    if (!lastPitchShiftFactor ||
+      Math.abs(lastPitchShiftFactor - pitchFactor) >= .15) {
+      pitchFactorParam.setValueAtTime(pitchFactor, audioContext.currentTime);
+    }
+    lastPitchShiftFactor = pitchFactor;
+  }
 }
 
 function findHeight(feature) {
@@ -424,4 +435,10 @@ function findHeight(feature) {
     maxY = Math.max(maxY, feature[i]._y);
   }
   return maxY - minY;
+}
+
+function setWahWahFreq(value) {
+  const maxFreq = 2200;
+  const newFreq = maxFreq * value;
+  wahWahNode.frequency.setValueAtTime(newFreq, audioContext.currentTime);
 }
